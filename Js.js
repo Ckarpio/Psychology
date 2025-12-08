b7db053ca1423e2dfa496e6e42b084dff674994b
Alexander Afonin <afonin@postmodern.ru>
2025-12-08 17:30:18 +0300

    fix issues of day one

--- Js.js
+++
@@ -1,9 +0,0 @@
-const NameEmo = "Гнев"
-const  DescriEmo = "Гнев — это энергия. Он показывает, что твои границы были нарушены. Важно не подавлять его, а научиться выражать конструктивно" 
-let intensityEmo = 6
-let powerEmo = true 
-console.log(NameEmo, DescriEmo, intensityEmo, powerEmo)
-Number(intensityEmo)
-Boolean(powerEmo)
-console.log(typeof(NameEmo), typeof(DescriEmo), typeof(intensityEmo), typeof(powerEmo))
-
