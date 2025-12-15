f4c3b10540df3c1533e12f0d6c90c7d3ed61c3ac
Andrey <zybik2522@gmail.com>
2025-12-15 21:30:00 +0300

    /

--- src/js_day_2.js
+++ src/js_day_2.js
@@ -1,15 +1,18 @@
 const EmoName = "злость"   
 let EmoIntensiv = 5 
 if (!EmoName){
+    console.error("Эмоция не выбрана")
     return
 }
 else{
     if(EmoIntensiv==Number){
         if(EmoIntensiv>=5){
             alert("Cовет")
+            console.log("рабоает")
         }   
     }
     else{
         return
+        
     }
 }
