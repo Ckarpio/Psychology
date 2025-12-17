e14ebb17fe09969863ec6c24b12c4829d732752f
Andrey <zybik2522@gmail.com>
2025-12-17 19:49:16 +0300

    Console.log

--- src/js/js_day_2.js
+++ src/js/js_day_2.js
@@ -1,15 +1,19 @@
 const EmoName = "злость"   
-let EmoIntensiv = 5 
-if (!EmoName){
+let EmoIntensiv = 5
+//const EmoName = 5  
+//let EmoIntensiv = "Пять"
+if (typeof(EmoName)!="string"){
+    console.error("Эмоция не найдена")
     return
 }
 else{
-    if(EmoIntensiv==Number){
+    if(typeof(EmoIntensiv)=="number"){
         if(EmoIntensiv>=5){
-            alert("Cовет")
+            console.log("Coвет")
         }   
     }
     else{
+        console.error("Интесивность не найдена")
         return
     }
 }
