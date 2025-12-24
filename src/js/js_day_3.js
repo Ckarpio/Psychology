b41d1d6f58b174c6126e33d128159365b992309c
Andrey <zybik2522@gmail.com>
2025-12-24 20:35:36 +0300

    js3

--- src/js/js_day_3.js
+++ src/js/js_day_3.js
@@ -2,15 +2,13 @@ const negativEmo=["Злость", "Грусть", "Паника", "Одиноч�
 const poistivEmo=["Радость", "Уверенность", "Любовь"]
 const Emo = negativEmo.concat(poistivEmo)
 const a = "Эмоция:"
-console.log(Emo.length, "Все эмоции")
-console.log(negativEmo.length, "Негативные ")
-
+console.log(negativEmo.join("  "))
 console.log(a.concat(negativEmo))
 negativEmo.forEach(a => {
     console.log("Эмоция:", a )
 });
-
-
+const symbol = negativEmo.map(n => n.length)
+console.log(symbol.reduce((prev,current)=> prev+=current))
 
 
 
