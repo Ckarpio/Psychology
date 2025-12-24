4ef843b4cd848977c952c9a08191b0e482a4b5ef
Andrey <zybik2522@gmail.com>
2025-12-24 21:39:10 +0300

    js3

--- src/js/js_day_3.js
+++ src/js/js_day_3.js
@@ -1,21 +1,15 @@
-const negativEmo=["Злость", "Грусть", "Паника", "Одиночество", "Стресс", "Тревога"]
-const poistivEmo=["Радость", "Уверенность", "Любовь"]
-const Emo = negativEmo.concat(poistivEmo)
-const a = "Эмоция:"
-console.log(a.concat(negativEmo))
-negativEmo.forEach(a => {
-    console.log("Эмоция:", a )
-});
-const symbol = negativEmo.map(n => n.length)
-console.log(symbol.reduce((prev,current)=> prev+=current))
+    const negativEmo=["Злость", "Грусть", "Паника", "Одиночество", "Стресс", "Тревога"]
+    const poistivEmo=["Радость", "Уверенность", "Любовь"]
+    const Emo = negativEmo.concat(poistivEmo)
+    const a = "Эмоция:"
+    console.log(a.concat(negativEmo))
+    negativEmo.forEach(a => {
+        console.log("Эмоция:", a )
+    });
+    const symbol = negativEmo.map(n => n.length)
+    console.log(symbol.reduce((prev,current)=> prev+=current))
 
-
-
-
-
-
-
-
+console.log(negativEmo.filter([i]%2===0))
 
 
 
