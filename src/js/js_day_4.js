5d423247c87cf2868924260372fd5f75280c81fa
Andrey <zybik2522@gmail.com>
2025-12-31 20:22:19 +0300

    js_day_4

---
+++ src/js/js_day_4.js
@@ -0,0 +1,30 @@
+const Emo ={
+    name: "Гнев",
+    type: "negative",
+    color: "red",
+    material:{
+        music: ["Beatles"],
+        video:[],
+        exercises:[],
+        articles:[],
+    }   
+
+}
+
+Emo.intensiv= 24 // Создание нового элемента
+
+delete Emo.intensiv // Удаление элемента
+
+Emo.color="black" // Изменение элемента
+
+if(Emo.type="negative"){
+    console.log(Emo.material) // Цикл
+}
+
+let intensiv = Object.assign({}, Emo) // Дублирование объекта
+
+console.log(intensiv)
+
+console.log(Emo)
+
+console.log(Emo.material.music)
