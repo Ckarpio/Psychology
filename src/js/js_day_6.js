541c893c4de9e07e7528e50881d30b53570ea27e
Andrey <zybik2522@gmail.com>
2026-01-08 13:45:36 +0300

    try/catch

--- src/js/js_day_6.js
+++ src/js/js_day_6.js
@@ -3,14 +3,35 @@ import {emotionsProperties} from "./const/emotionProperties.js"
 
 
 function findEmo(emotionsCode){
+    try{
     return emotions.find(emotions=> emotions.code==emotionsCode)
+    }
+    catch(error){
+        console.error("Ошибка при посике")
+        return null
+    }
+    
+
+
 }
 
 function findEmoProperties(emotions){
+    try{
+        if (!emotions || !emotions.code){
+            console.error("Неверный объект")
+        }
+    
     return emotionsProperties.find(property => property.code===emotions.code)
+    }
+    catch(error){
+        console.error("Ошибка поиска свойств" )
+        return nyll
+    }
+
 } 
 
 function main(){
+    try{
     let emoCode="joy"
     let foundEmotions = findEmo(emoCode)
     if (!foundEmotions){
@@ -27,8 +48,15 @@ function main(){
     
     console.log("Найденная эмоция:" , foundEmotions)
     console.log("Свойства эмоции:", foundProperties )
-    
+} catch(error){
+    console.error('Ошибка  приложения')
+}
 }
 
-
+try{
 main()
+}
+catch(error){
+    console.error("Ошибка запуска")
+
+}
