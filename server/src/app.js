039b49a3ede02987b431dae407a09ff1208da241
Alexander Afonin <afonin@postmodern.ru>
2026-02-25 20:40:08 +0300

    cors fix

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const { initDb } = require('./db');
 const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 initDb(); 
