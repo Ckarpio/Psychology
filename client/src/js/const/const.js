d093279c07518f372f5237a877425805bbe7c56a
Alexander Afonin <afonin@postmodern.ru>
2026-03-05 11:27:56 +0300

    new path for render

--- client/src/js/const/const.js
+++ client/src/js/const/const.js
@@ -1,2 +1,1 @@
-// TODO: заменить на реальный URL после создания Web Service на render.com
-export const API_URL = 'https://psychology-api.onrender.com';
+export const API_URL = 'https://psychology-1hac.onrender.com/';
